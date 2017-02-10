106a494e8f7cf406e53b374acef6d92024ebc357
Hussain Haq <hussain@Hussains-MacBook-Air.local>
2017-02-10 10:23:08 -0600

    Initial Commit

--- Multiply App/ViewController.swift
+++ Multiply App/ViewController.swift
@@ -9,17 +9,92 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var enterTextField1: UITextField!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var enterTextField2: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var box1 = 0
+    var box2 = 0
+    var answer = 0
+    var even = 0
+    var odd = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func Division(sender: AnyObject) {
+        print ("division")
+        
+        enterTextField1.resignFirstResponder()
+        enterTextField2.resignFirstResponder()
+        let box1 = Int (enterTextField1.text!)
+        let box2 = Int (enterTextField2.text!)
+        
+        answer = box1! / box2!
+        answerLabel.text = "\(answer)"
     }
+    
+    
+    @IBAction func Subtraction(sender: AnyObject) {
+        print ("subtract")
+        
+        enterTextField1.resignFirstResponder()
+        enterTextField2.resignFirstResponder()
+        let box1 = Int (enterTextField1.text!)
+        let box2 = Int (enterTextField2.text!)
+        
+        answer = box1! - box2!
+        answerLabel.text = "\(answer)"
+    }
+    
+    
+    
+    
+    @IBAction func add(sender: AnyObject) {
+        print ("Addition")
+        
+        enterTextField1.resignFirstResponder()
+        enterTextField2.resignFirstResponder()
+        let box1 = Int (enterTextField1.text!)
+        let box2 = Int (enterTextField2.text!)
+        
+        answer = box1! + box2!
+        answerLabel.text = "\(answer)"
+    }
+    
+    @IBAction func multiply(sender: AnyObject) {
+        print ("Multiply")
+        
+        enterTextField1.resignFirstResponder()
+        enterTextField2.resignFirstResponder()
+        let box1 = Int (enterTextField1.text!)
+        let box2 = Int (enterTextField2.text!)
+        
+        answer = box1! * box2!
+        answerLabel.text = "\(answer)"
+        
+        if answer == 64 {
+            imageView.image = UIImage (named:"Mario Kart")
+            print("Activated")
+        }
+        else if answer % 2 == 0 {
+            print("even")
+            imageView.image = UIImage (named:"funnyImage")
+            answer == even * 2
+        }
+            
+        else if answer % 2 == 1{
+            print("odd")
+            imageView.image = UIImage (named:"funny")
+        
+            
+        
+            }
+        }
+   }
 
 
-}
 
